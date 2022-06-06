5e890f5ab0003b1614789d9a9a4c9b89139f1272
Iwanmuarifn <iwan.muarifin1@gmail.com>
2022-06-06 21:30:10 +0700

    2 commit

---
+++ app/src/main/java/org/d3if0100/hilang_dan_temukan/LoginFragment.kt
@@ -0,0 +1,19 @@
+package org.d3if0100.hilang_dan_temukan
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
